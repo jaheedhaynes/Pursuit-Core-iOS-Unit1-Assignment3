49c943fc6f0d68cc1654820bfbfe18a27ba9f170
Jaheed Haynes <jaheedhaynes@Jaheeds-MacBook-Pro.local>
2019-10-15 10:03:10 -0400

    update

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -28,6 +28,27 @@ var closureOperation = mathStuffFactory(opString: "+")
 var result = closureOperation(45, 5)
 print("result of operation is \(result)")
 
+//mathStuffFactory(opString: <#T##String#>)
+
+
+//========================================================================================================================
+
+var addition = "0.0 + 0.0"
+var additionArray = addition.components(separatedBy: " ") // seperating String into individual elements in an array
+
+var addEquationElement1 = Double(additionArray[0]) //converting String "0.0" to
+var addEquationElement2 = Double(additionArray[2]) //here number is of type "Int?"
+
+//using Forced Unwrapping
+
+if (addEquationElement1 != nil) && addEquationElement2 != nil {
+ //string is converted to Int
+}
+
+print(additionArray)
+
+
+
 
 
 //========================================================================================================================
@@ -36,37 +57,74 @@ print("Hello, my name is Terminator & I'm the Most powerful calculator on the PL
 
 print("Enter type of calculation you want me to perform, Enter 1 (regular) or 2 (high order)")
 
-var calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 1
-
-
-repeat {
+let calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 1
     
-    if calculationType == 1 {
-        closureOperation = mathStuffFactory(opString.random():"") {
-            if "+" {
-                print("What TWO doubles would you like to ADD")
-                closureOperation = mathStuffFactory(opString: "+")
-                calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 100
-                
-            } else if "-" {
-                print("What TWO doubles numbers would you like to SUBTRACT")
-                closureOperation = mathStuffFactory(opString: "-")
-                calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 100
-            }
-            else if "*" {
-                print("What TWO doubles numbers would you like to MULTIPLY")
-                closureOperation = mathStuffFactory(opString: "*")
-                calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 100
-            }
-            else if "/" {
-                print("What TWO doubles numbers would you like to DIVIDE")
-                closureOperation = mathStuffFactory(opString: "/")
-                calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 100
-            }
-            
-            
-            
-        }
-    }
+    switch calculationType {
+    case 1 :
+        print("Enter your SIMPLE EQUATION (TWO NUMBERS ONLY)")
+    default:
+        print("ERROR, CHOOSE EITHER 1 or 2")
+        
+        
 }
+   
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        if "+"  {
+//            //print("What TWO doubles would you like to ADD")
+//            closureOperation = mathStuffFactory(opString: "+")
+//            calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 100
+//            //result = closureOperation(Double, Double)
+//
+//
+//        } else if "-" {
+//            //print("What TWO doubles numbers would you like to SUBTRACT")
+//            closureOperation = mathStuffFactory(opString: "-")
+//            calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 100
+//        }
+//        else if "*" {
+//            //print("What TWO doubles numbers would you like to MULTIPLY")
+//            closureOperation = mathStuffFactory(opString: "*")
+//            calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 100
+//        }
+//        else if "/" {
+//            //print("What TWO doubles numbers would you like to DIVIDE")
+//            closureOperation = mathStuffFactory(opString: "/")
+//            calculationType = Int(readLine() ?? "Pick a valid Integer") ?? 100
+//        }
+//
+//    }
+//while true
 
